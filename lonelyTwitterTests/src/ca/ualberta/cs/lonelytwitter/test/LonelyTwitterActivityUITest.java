09d0d44190fece79318a03e88b9bbae59ef429d6
alisajedi <alisajedi@yahoo.com>
2014-10-19 18:36:43 -0600

    Update LonelyTwitterActivityUITest.java

--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
@@ -35,20 +35,6 @@ public class LonelyTwitterActivityUITest extends
 		((Button) activity.findViewById(ca.ualberta.cs.lonelytwitter.R.id.save)).performClick();
 	}
 	
-	//To avoid error: "CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views."
-	//That's because "Activities" run on the UI thread. Using this annotation, all the method statements will run on the UI thread:
-	@UiThreadTest
-	public void testMakeTweet(){
-		LonelyTwitterActivity activity = getActivity(); //Get the Activity under test, starting it if necessary. For each test method invocation, the Activity will not actually be created until the first time this method is called
-		int old_length = activity.getAdapter().getCount();
-
-		makeTweet("Hello...");
-		ArrayAdapter<NormalTweetModel> adapter = activity.getAdapter();
-		assertEquals("Length of array adapter is not right!", old_length+1, adapter.getCount());
-		
-		assertEquals("Did you add NormalTweetModel? It's not that!", true, (adapter.getItem(adapter.getCount() - 1) instanceof NormalTweetModel));
-		
-		NormalTweetModel tweet = adapter.getItem(adapter.getCount() - 1);
-		assertEquals("This is not the text we expected!", tweet.getText(), "Hello...");		
-	}
+//TODO: Add your code here:
+			
 }
